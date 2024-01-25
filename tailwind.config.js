6b510f77f74927126c9ff5558456e007e15d6a66
sndpjaat <sndpjaat008@gmail.com>
2024-01-25 11:54:35 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".//**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Rubik' : ['Rubik' , 'sans-serif'],
+
+        'Inter' : ['Inter' , 'sans-serif']
+
+      
+
+      }
+    },
+  },
+  plugins: [],
+}
+
